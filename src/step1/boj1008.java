52d0078ece87a437098b1cc0fb8bfce6da33ba9a
이승준 <klsj9810@gmail.com>
2024-03-25 23:54:43 +0900

    boj1008

---
+++ src/step1/boj1008.java
@@ -0,0 +1,18 @@
+package step1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj1008 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+
+        System.out.println(a/b);
+    }
+}
