19088419902eb1d0a4cb82caa8bc9b1c629cd428
이승준 <klsj9810@gmail.com>
2024-04-02 22:39:38 +0900

    boj11382

---
+++ src/step1/boj10171.java
@@ -0,0 +1,17 @@
+package step1;
+
+public class boj10171 {
+    public static void main(String[] args) {
+        /*
+        *     \    /\
+               )  ( ')
+              (  /  )
+               \(__)|
+        *
+        * */
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
